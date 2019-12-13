3a6cab525f0623da7dc788fc513a26a2fa672356
rick325601 <56115492+rick325601@users.noreply.github.com>
2019-12-13 15:15:50 +0100

    no message

---
+++ Assets/scipts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+    private float xRange = 15.0f;
+    private float zRange = 10.0f;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+        
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange );
+        }
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange );
+        }
+
+    }
+
+    public class Rotator : MonoBehaviour
+    {
+        void Update()
+        {
+            transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+            }
+    }
+}
